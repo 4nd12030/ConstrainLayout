6a0d42db500b5fe4f22cd88fcc3ce46669f8dc8c
Andrea Aguiire <4ndr3420@gmail.com>
2023-08-22 16:40:22 -0600

    Columnas por ancho de pantalla

--- app/src/main/java/com/example/constrainlayout/NotaFragment.java
+++ app/src/main/java/com/example/constrainlayout/NotaFragment.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,8 +69,12 @@ public class NotaFragment extends Fragment {
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
+                DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+                float dpWith = displayMetrics.widthPixels / displayMetrics.density;
+                int numeroColumnas = (int) (dpWith /180);
                 //recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(mColumnCount, StaggeredGridLayoutManager.VERTICAL));
+                //recyclerView.setLayoutManager(new StaggeredGridLayoutManager(mColumnCount, StaggeredGridLayoutManager.VERTICAL));
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(numeroColumnas, StaggeredGridLayoutManager.VERTICAL));
             }
             //recyclerView.setAdapter(new MyNotaRecyclerViewAdapter(notaList, mListener));
 
@@ -92,6 +97,18 @@ public class NotaFragment extends Fragment {
                     "page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web " +
                     "sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on" +
                     " purpose (injected humour and the like).\n", true, android.R.color.holo_purple));
+            notaList.add(new Nota("Lista de la compra de la semana", "Comprar tostado", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Recordar", "He aparcado el coche en la calle Republica, no olvidarme de pagar el parquimetro", false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Fiesta cumpleanos de Margaret","Contrary to popular belief, Lorem Ipsum is not simply random text. It has " +
+                    "roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor " +
+                    "at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, " +
+                    "and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections " +
+                    "1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is" +
+                    " a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", " +
+                    "comes from a line in section 1.10.32.",false, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Lista de la compra de la semana", "Comprar tostado", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Recordar", "He aparcado el coche en la calle Republica, no olvidarme de pagar el parquimetro", false, android.R.color.holo_green_light));
+
 
             adapterNotas = new MyNotaRecyclerViewAdapter(notaList, mListener);
             recyclerView.setAdapter(adapterNotas);
