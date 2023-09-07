93ea6c31d257aed6e67192c4cc07acd9e9213ec2
Andrea Aguiire <4ndr3420@gmail.com>
2023-09-07 17:49:30 -0600

    Creacion de una instancia de la base de datos

---
+++ app/src/main/java/com/example/constrainlayout/NotaRoomDatabase.java
@@ -0,0 +1,26 @@
+package com.example.constrainlayout;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {NotaEntity.class}, version = 1)
+public abstract class NotaRoomDatabase extends RoomDatabase {
+    public abstract NotaDao notaDao();
+    private static volatile NotaRoomDatabase INSTANCE;
+
+    public static NotaRoomDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (NotaRoomDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NotaRoomDatabase.class, "nota_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
